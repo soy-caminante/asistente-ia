ce881cbe5d610f3b9d80b4c04279f3822b4708a2
Pepe <pepe.sevilla@orbitare.space>
2025-05-10 10:06:02 +0200

    ajustes

--- yukai/runtime/vllm/vllm-test.sh
+++ yukai/runtime/vllm/vllm-test.sh
@@ -5,7 +5,6 @@ export HF_TOKEN="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"
 python3 -m vllm.entrypoints.openai.api_server \
   --model meta-llama/Llama-3.1-8B-Instruct \
   --tokenizer meta-llama/Llama-3.1-8B-Instruct \
-  --gpu-memory-utilization 0.9 \
   --max-model-len 4096 \
-  --quantization bitsandbytes \
-  --port 8081
+  --dtype float32 \
+  --port 8000
